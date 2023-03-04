3b13899d0882e3c5f0194039547f7ae2e263f1b4
Marek Kotarba <markuskotarba@gmail.com>
2023-03-04 14:17:19 +0100

    adding display protein (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const [sequence, setSequence] = useState(null);
+  const [sequence, setSequence] = useState("");
   return (
     <BrowserRouter>
       <Routes>
--- src/views/Results/Results.jsx
+++ src/views/Results/Results.jsx
@@ -5,6 +5,7 @@ import searchOutline from "./../../assets/Results/search-outline.svg"
 import eyeOutline from "./../../assets/Results/eye-outline.svg"
 import downloadOutline from "./../../assets/Results/download-outline.svg"
 import dnaImage from "./../../assets/MainPage/dna.png"
+import Protein from '../../components/Protein/Protein'
 
 /*
 interface IPorotein: {
@@ -18,6 +19,7 @@ const Results = ({ sequence }) => {
   const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
   const [currentShift, setCurrentShift] = useState(1);
   const [currentProtein, setCurrentProtein] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
     const fetchData = async () => {
       const data = await invoke("rdFromKbrd", {sekwencja: sequence});
@@ -26,6 +28,16 @@ const Results = ({ sequence }) => {
     fetchData();
   }, [currentShift]);
 
+  // console.log(sequence, currentProtein)
+  // console.log(currentIndex)
+
+  const buttons = []
+  for (let i = 0; i < currentProtein.length; i++)
+    buttons.push(i)
+
+  const setIndex = () => {
+    setCurrentIndex(parseInt(document.querySelector('input[name="results"]:checked').value) - 1)
+  }
   
   return (
     <div className='results'>
@@ -40,26 +52,9 @@ const Results = ({ sequence }) => {
         <p className='results__title'>Białka</p>
         <div className='results__menu-line'>
           <div id='results__menu'>
-            <label><input type="radio" name='results'/><span>1.</span></label>
-            <label><input type="radio" name='results'/><span>2.</span></label>
-            <label><input type="radio" name='results'/><span>3.</span></label>
-            <label><input type="radio" name='results'/><span>4.</span></label>
-            <label><input type="radio" name='results'/><span>5.</span></label>
-            <label><input type="radio" name='results'/><span>6.</span></label>
-            <label><input type="radio" name='results'/><span>7.</span></label>
-            <label><input type="radio" name='results'/><span>8.</span></label>
-            <label><input type="radio" name='results'/><span>9.</span></label>
-            <label><input type="radio" name='results'/><span>10.</span></label>
-            <label><input type="radio" name='results'/><span>11.</span></label>
-            <label><input type="radio" name='results'/><span>12.</span></label>
-            <label><input type="radio" name='results'/><span>13.</span></label>
-            <label><input type="radio" name='results'/><span>14.</span></label>
-            <label><input type="radio" name='results'/><span>15.</span></label>
-            <label><input type="radio" name='results'/><span>16.</span></label>
-            <label><input type="radio" name='results'/><span>17.</span></label>
-            <label><input type="radio" name='results'/><span>18.</span></label>
-            <label><input type="radio" name='results'/><span>19.</span></label>
-            <label><input type="radio" name='results'/><span>20.</span></label>
+            {buttons.map((el, index) => {
+              return <label><input type="radio" name='results' onClick={setIndex} value={el + 1}/><span>{el + 1}.</span></label>
+            })}
           </div>
         </div>
       </div>
@@ -71,7 +66,7 @@ const Results = ({ sequence }) => {
       <div className="results__window">
         <div className="results__window__header">
           <h2>Sekwencja kodu białka</h2>
-          <p>KWTKICSLHSLPQS ...</p>
+          <p>{String(Object(currentProtein[currentIndex]).bialka).slice(0, 14)} ...</p>
           <div>
             <button><img src={eyeOutline} alt="" /></button>
             <button><img src={downloadOutline} alt="" /></button>
@@ -82,20 +77,14 @@ const Results = ({ sequence }) => {
             <div>
               <div className="results__window__box__mass">
                 <h2>Masa</h2>
-                <p>testtesttest * 10^-2137</p>
-              </div>
-              <div className="results__window__box__mass">
-                <h2>Masa</h2>
-                <p>testtesttest * 10^-2137</p>
-              </div>
-              <div className="results__window__box__mass">
-                <h2>Masa</h2>
-                <p>testtesttest * 10^-2137</p>
+                <p>{Object(currentProtein[currentIndex]).waga}</p>
               </div>
             </div>
             <button className='results__window__btn'>Zobacz Wykresy I Diagramy</button>
           </div>
-          
+          <div className="results__window__right" id='protein'>
+            <Protein formula={Object(currentProtein[currentIndex]).bialka}/>
+          </div>
         </div>
       </div>
     </div>
